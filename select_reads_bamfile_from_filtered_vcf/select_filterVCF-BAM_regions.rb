89767b6bc94db6380ffa99726ec1abd96bd75cff
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2014-02-12 11:00:13 +0000

    code cleanup
    print header for the sam file being written, so that bam file can be created, sorted and indexed

--- select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
+++ select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
@@ -6,13 +6,8 @@ def extract_from_bam(line)
 	data = Hash.new {|h,k| h[k] = {} }
 	bamfile = ARGV[0]
 	array = line.split("\t")
-#	upper = array[2].to_i + 1
-#	lower = array[2].to_i - 1
-#	sam = system ("source", "samtools-0.1.19;", "samtools", "view", "bamfile", "#{array[1]}:#{lower}-#{upper}")
 	File.new("Reading-out.sam", "w")
-#	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam"
-#	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam]
-	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{array[2]}-#{array[2]} -o Reading-out.sam"
+#	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{array[2]}-#{array[2]} -o Reading-out.sam"
 	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{array[2]}-#{array[2]} -o Reading-out.sam]
 	sam = File.read("Reading-out.sam")
 	alignments = sam.split("\n")
@@ -24,15 +19,18 @@ def extract_from_bam(line)
 end
 
 outfile = File.new("Test-#{ARGV[0]}-out.sam", "w")
+headers = %x[source samtools-0.1.19; samtools view -H #{ARGV[0]}]
+outfile.puts headers
 
 lines = File.read(ARGV[1])
 results = lines.split("\n")
 results.each do |string|
   if string !~ /^#/
-	puts string
+#	puts string
 	hash = extract_from_bam(string)
 	hash.each { |key,value|
 		outfile.puts  hash[key]
 	}
   end
 end
+
