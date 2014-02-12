2a84b3dcab8bfe7e8a426c9decb3f0882ffaf650
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2014-02-12 10:45:12 +0000

    previously script was used to extract reads covering at least 5 bases up or down the alt position. Script modified to extract reads overlapping with the alt position only

--- select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
+++ select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
@@ -6,12 +6,14 @@ def extract_from_bam(line)
 	data = Hash.new {|h,k| h[k] = {} }
 	bamfile = ARGV[0]
 	array = line.split("\t")
-	upper = array[2].to_i + 5
-	lower = array[2].to_i - 5
-#	sam = system ("source", "samtools-0.1.19;", "samtools", "view", "bamfile", "#{array[1]}:#{upper}-#{lower}")
+#	upper = array[2].to_i + 1
+#	lower = array[2].to_i - 1
+#	sam = system ("source", "samtools-0.1.19;", "samtools", "view", "bamfile", "#{array[1]}:#{lower}-#{upper}")
 	File.new("Reading-out.sam", "w")
-	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam"
-	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam]
+#	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam"
+#	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam]
+	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{array[2]}-#{array[2]} -o Reading-out.sam"
+	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{array[2]}-#{array[2]} -o Reading-out.sam]
 	sam = File.read("Reading-out.sam")
 	alignments = sam.split("\n")
 	alignments.each do |string|
