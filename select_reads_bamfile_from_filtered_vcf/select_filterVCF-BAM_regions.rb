ab65caac76b5e517dac0ae842805308dcb52d22f
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2014-02-12 10:42:22 +0000

    fix integer issues and file opening and reading lines not starting with '#'

--- select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
+++ select_reads_bamfile_from_filtered_vcf/select_filterVCF-BAM_regions.rb
@@ -2,16 +2,17 @@
 # encoding: utf-8
 #
 
-
-bamfile = File.read(ARGV[0])
-
-def extract_from_bam (string)
+def extract_from_bam(line)
 	data = Hash.new {|h,k| h[k] = {} }
-	array = string.split("\t")
-	upper = array[2] + 5
-	lower = array[2] - 5
+	bamfile = ARGV[0]
+	array = line.split("\t")
+	upper = array[2].to_i + 5
+	lower = array[2].to_i - 5
 #	sam = system ("source", "samtools-0.1.19;", "samtools", "view", "bamfile", "#{array[1]}:#{upper}-#{lower}")
-	sam = %x[source samtools-0.1.19; samtools view bamfile array[1]:upper-lower]
+	File.new("Reading-out.sam", "w")
+	puts "source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam"
+	%x[source samtools-0.1.19; samtools view #{bamfile} #{array[1]}:#{lower}-#{upper} -o Reading-out.sam]
+	sam = File.read("Reading-out.sam")
 	alignments = sam.split("\n")
 	alignments.each do |string|
 		info = string.split("\t")
@@ -25,8 +26,11 @@ outfile = File.new("Test-#{ARGV[0]}-out.sam", "w")
 lines = File.read(ARGV[1])
 results = lines.split("\n")
 results.each do |string|
-	hash = extract_from_bam (string)
+  if string !~ /^#/
+	puts string
+	hash = extract_from_bam(string)
 	hash.each { |key,value|
 		outfile.puts  hash[key]
 	}
+  end
 end
