2330b653388a25299e7efdeb94459d5e73b67010
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2014-03-24 12:07:45 +0000

    script to print each user time stamp of a pattern saved

---
+++ selectsequences_cov_alt_allele/user_play_stats/user_visit_timeline/connect_mysql_db.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'active_record'
+require 'pp'
+
+### Start mysql if it has not started
+status = %x[mysql.server status]
+# warn (status)
+if status.chomp == ' ERROR! MySQL is not running'
+	status = %x[mysql.server start]
+	# warn (status)
+end
+
+### Database config
+ActiveRecord::Base.establish_connection(
+	:adapter => "mysql",
+	:database => "atd",
+	:username => "root",
+	:password => "user",
+	:host => "localhost"
+	)
+
+### Schema
+class Cigar < ActiveRecord::Base
+	self.table_name = "cigar"
+end
+
+class Dataset < ActiveRecord::Base
+	self.table_name = "dataset"
+end
+
+class Notification < ActiveRecord::Base
+	self.table_name = "notification"
+end
+
+class Pattern < ActiveRecord::Base
+	self.table_name = "pattern"
+end
+
+class User < ActiveRecord::Base
+	self.table_name = "user"
+end
+
+# rename column name "type" to "relations" in Notification table
+=begin
+class Tablemodify < ActiveRecord::Migration
+	rename_column(:notification, :type, :relations)
+end
+
+=end
