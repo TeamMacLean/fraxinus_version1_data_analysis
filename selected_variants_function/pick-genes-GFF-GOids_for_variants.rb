574125aee5f6c99950b2e0f6e903678d0a857b72
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2014-11-27 14:03:29 +0000

    prints out selected genes for each variant to a file

--- selected_variants_function/pick-genes-GFF-GOids_for_variants.rb
+++ selected_variants_function/pick-genes-GFF-GOids_for_variants.rb
@@ -29,7 +29,7 @@ gff2.records.each do | record |
 	if record.feature == 'gene'
 		geneid = record.get_attributes('ID').join(" ").gsub(/\.\d$/, '')
 		limits = [record.start, record.end].join("_")
-		contigs[geneid] = limits
+		contigs[record.seqid][geneid] = limits
 	end
 end
 
@@ -44,31 +44,36 @@ lines.split("\n").each do |line|
 		array1 = line.split("\t")
 		if (array1[11].to_f > array1[10].to_f) and (array1[9].to_f >= array1[8].to_f)
 			term = [array1[0], array1[1], array1[2], array1[3]].join("_")
-			variants[term] = array1[3]
+			variants[array1[1]][term] = array1[3]
 		end
 	end
 end
 
 candis = Hash.new {|h,k| h[k] = Hash.new {|h,k| h[k] ={} } }
-variants.each_key { |selected|
-	contigs.each_key { |gene|
-		coord = contigs[gene].split("_")
-		if (variants[selected].to_i >= coord[0].to_i) and (variants[selected].to_i <= coord[0].to_i)
-			candis[selected][gene] = 1
-		end
+variants.each_key { |contig|
+	variants[contig].each_key { |selected|
+		contigs[contig].each_key { |gene|
+			coord = contigs[contig][gene].split("_")
+			warn "#{gene}\t#{coord[0].to_i}\t#{coord[1].to_i}"
+			if (variants[contig][selected].to_i >= coord[0].to_i) and (variants[contig][selected].to_i <= coord[1].to_i)
+				candis[selected][gene] = 1
+			end
+		}
 	}
 }
 
 ## New file is opened to write the selected variants info
 outfile = File.new("Selected_gene_goids.txt", "w")
-variants.each_key { |selected2|
-	outfile.print "#{selected2}"
-	if candis.has_key?(selected2)
-		candis[selected2].each_key { |terms|
-			outfile.print "\t#{terms}"
-		}
-		outfile.print "\n"
-	else
-		outfile.print "\tNo genes\n"
-	end
+variants.each_key { |contig2|
+	variants[contig2].each_key { |selected2|
+		outfile.print "#{selected2}"
+		if candis.has_key?(selected2)
+			candis[selected2].each_key { |terms|
+				outfile.print "\t#{terms}"
+			}
+			outfile.print "\n"
+		else
+			outfile.print "\tNo genes\n"
+		end
+	}
 }
